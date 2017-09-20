ddc969c41a4b46e1fcc98818de11833298bc5ade
dpoliszak <dawid.poliszak@lptg.pl>
2017-09-20 16:56:05 +0200

    Configure webpack build application

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+import webpack from 'webpack';
+import path from 'path';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({hot: true, template: './src/index.html', filename: 'index.html', inject: 'body'})
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options :  {
+                    presets  :  [ 'es2017'] 
+                  } 
+            }, {
+                test: /\.jsx$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            { test: /\.css$/, loader: 'style!css' },
+            { test: /\.(png|jpg|jpeg|gif|woff)$/, loader: 'url?limit=8192' },
+            { test: /\.(otf|eot|ttf)$/, loader: 'file?prefix=font/' },
+            { test: /\.svg$/, loader: 'file' }
+        ]
+    },
+    devServer: {
+        host: 'localhost',
+        port: 3001,
+    
+        historyApiFallback: true,
+        // respond to 404s with index.html
+    
+        hot: true,
+        // enable HMR on the server
+      },
+    plugins : [
+        HtmlWebpackPluginConfig,
+        new webpack.HotModuleReplacementPlugin(),
+    ]
+};
+
+
