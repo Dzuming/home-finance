c3832124f77f72b5034907f4ec9213f2aa5b41a2
Dawid Poliszak <dawidpoliszak@karpas.pl>
2017-09-21 21:38:13 +0200

    Add sorting to table

--- src/containers/Table.js
+++ src/containers/Table.js
@@ -1,52 +1,114 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import {
-  // State or Local Processing Plugins
+  SortingState, LocalSorting
 } from '@devexpress/dx-react-grid';
-import {
-  Grid,
-  TableView,
-  TableHeaderRow
-} from '@devexpress/dx-react-grid-material-ui';
+import {Grid, TableView, TableHeaderRow} from '@devexpress/dx-react-grid-material-ui';
 
-class Table extends React.PureComponent {
+class Table extends PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
       columns: [
-        { name: 'name', title: 'Name' },
-        { name: 'sex', title: 'Sex' },
-        { name: 'city', title: 'City' },
-        { name: 'car', title: 'Car' }
+        {
+          name: 'name',
+          title: 'Name'
+        }, {
+          name: 'sex',
+          title: 'Sex'
+        }, {
+          name: 'city',
+          title: 'City'
+        }, {
+          name: 'car',
+          title: 'Car'
+        }
       ],
       rows: [
-        { sex: "Female", name: "Sandra", city: "Las Vegas", car: "Audi A4" },
-        { sex: "Male", name: "Paul", city: "Paris", car: "Nissan Altima" },
-        { sex: "Male", name: "Mark", city: "Paris", car: "Honda Accord" },
-        { sex: "Male", name: "Paul", city: "Paris", car: "Nissan Altima" },
-        { sex: "Female", name: "Linda", city: "Austin", car: "Toyota Corolla" },
-        { sex: "Male", name: "Robert", city: "Las Vegas", car: "Chevrolet Cruze" },
-        { sex: "Female", name: "Lisa", city: "London", car: "BMW 750" },
-        { sex: "Male", name: "Mark", city: "Chicago", car: "Toyota Corolla" },
-        { sex: "Male", name: "Thomas", city: "Rio de Janeiro", car: "Honda Accord" },
-        { sex: "Male", name: "Robert", city: "Las Vegas", car: "Honda Civic" },
-        { sex: "Female", name: "Betty", city: "Paris", car: "Honda Civic" },
-        { sex: "Male", name: "Robert", city: "Los Angeles", car: "Honda Accord" },
-        { sex: "Male", name: "William", city: "Los Angeles", car: "Honda Civic" },
-        { sex: "Male", name: "Mark", city: "Austin", car: "Nissan Altima" }
+        {
+          sex: "Female",
+          name: "Sandra",
+          city: "Las Vegas",
+          car: "Audi A4"
+        }, {
+          sex: "Male",
+          name: "Paul",
+          city: "Paris",
+          car: "Nissan Altima"
+        }, {
+          sex: "Male",
+          name: "Mark",
+          city: "Paris",
+          car: "Honda Accord"
+        }, {
+          sex: "Male",
+          name: "Paul",
+          city: "Paris",
+          car: "Nissan Altima"
+        }, {
+          sex: "Female",
+          name: "Linda",
+          city: "Austin",
+          car: "Toyota Corolla"
+        }, {
+          sex: "Male",
+          name: "Robert",
+          city: "Las Vegas",
+          car: "Chevrolet Cruze"
+        }, {
+          sex: "Female",
+          name: "Lisa",
+          city: "London",
+          car: "BMW 750"
+        }, {
+          sex: "Male",
+          name: "Mark",
+          city: "Chicago",
+          car: "Toyota Corolla"
+        }, {
+          sex: "Male",
+          name: "Thomas",
+          city: "Rio de Janeiro",
+          car: "Honda Accord"
+        }, {
+          sex: "Male",
+          name: "Robert",
+          city: "Las Vegas",
+          car: "Honda Civic"
+        }, {
+          sex: "Female",
+          name: "Betty",
+          city: "Paris",
+          car: "Honda Civic"
+        }, {
+          sex: "Male",
+          name: "Robert",
+          city: "Los Angeles",
+          car: "Honda Accord"
+        }, {
+          sex: "Male",
+          name: "William",
+          city: "Los Angeles",
+          car: "Honda Civic"
+        }, {
+          sex: "Male",
+          name: "Mark",
+          city: "Austin",
+          car: "Nissan Altima"
+        }
       ]
     };
+    this.changeSorting = sorting => this.setState({sorting});
   }
   render() {
-    const { rows, columns } = this.state;
+    const {rows, columns} = this.state;
 
     return (
-      <Grid
-        rows={rows}
-        columns={columns}
-        >
-        <TableView />
-        <TableHeaderRow />
+      <Grid rows={rows} columns={columns}>
+        <SortingState />
+        <LocalSorting/>
+        <TableView/>
+        <TableHeaderRow allowSorting/>
       </Grid>
     );
   }
