80f2e22e17f7208c22ce4c026578ca26d0a37b9b
Dawid Poliszak <dawidpoliszak@karpas.pl>
2017-09-22 04:52:54 +0200

    WIP: add table

--- src/containers/Table.js
+++ src/containers/Table.js
@@ -1,7 +1,68 @@
 import React, {PureComponent} from 'react';
-import {SortingState, LocalSorting, PagingState, LocalPaging} from '@devexpress/dx-react-grid';
-import {Grid, TableView, TableHeaderRow, PagingPanel} from '@devexpress/dx-react-grid-material-ui';
+import PropTypes from 'prop-types';
+import {withStyles} from 'material-ui/styles';
+import {
+  SortingState,
+  LocalSorting,
+  PagingState,
+  LocalPaging,
+  FilteringState,
+  LocalFiltering,
+  EditingState
+} from '@devexpress/dx-react-grid';
+import {TableCell, IconButton} from 'material-ui';
+import {
+  Grid,
+  TableView,
+  TableHeaderRow,
+  PagingPanel,
+  TableFilterRow,
+  TableEditRow,
+  TableEditColumn,
+  DropDownMenu
+} from '@devexpress/dx-react-grid-material-ui';
+import EditIcon from 'material-ui-icons/Edit';
+const styles = theme => ({
+  commandButton: {
+    minWidth: '40px'
+  },
+  lookupEditCell: {
+    verticalAlign: 'middle',
+    paddingRight: theme.spacing.unit,
+    '& ~ $lookupEditCell': {
+      paddingLeft: theme.spacing.unit
+    }
+  },
+  dialog: {
+    width: 'calc(100% - 16px)'
+  }
+});
+const commandTemplates = {
+  edit: onClick => (
+    <IconButton onClick={onClick} title="Edit row">
+      <EditIcon/>
+    </IconButton>
+  )
+};
+const LookupEditCellBase = (({value, onValueChange, availableValues, classes}) => (
+  <TableCell className={classes.lookupEditCell}>
+    <DropDownMenu
+      onItemClick={newValue => onValueChange(newValue)}
+      defaultTitle={value}
+      items={availableValues}/>
+  </TableCell>
+));
+LookupEditCellBase.propTypes = {
+  value: PropTypes.any,
+  onValueChange: PropTypes.func.isRequired,
+  availableValues: PropTypes.array.isRequired,
+  classes: PropTypes.object.isRequired
+};
+LookupEditCellBase.defaultProps = {
+  value: undefined
+};
 
+export const LookupEditCell = withStyles(styles, {name: 'ControlledModeDemo'})(LookupEditCellBase);
 class Table extends PureComponent {
   constructor(props) {
     super(props);
@@ -94,25 +155,53 @@ class Table extends PureComponent {
           city: "Austin",
           car: "Nissan Altima"
         }
-      ]
+      ],
+      editingRows: []
     };
-    this.changeSorting = sorting => this.setState({sorting});
+    this.changeEditingRows = editingRows => this.setState({editingRows})
   }
   render() {
     const {rows, columns} = this.state;
 
     return (
       <Grid rows={rows} columns={columns}>
+        <FilteringState defaultFilters={[]}/>
         <PagingState defaultCurrentPage={0} pageSize={10}/>
         <SortingState/>
-        <LocalPaging />
+        <EditingState onCommitChanges={this.commitChanges}/>
+        <LocalFiltering/>
+        <LocalPaging/>
         <LocalSorting/>
         <TableView/>
         <TableHeaderRow allowSorting/>
+        <TableFilterRow/>
+        <TableEditRow
+          editCellTemplate={(props) => {
+          const {column} = props;
+          const columnValues = availableValues[column.name];
+          if (columnValues) {
+            return <LookupEditCell {...props} availableValues={columnValues}/>;
+          }
+          return undefined;
+        }}/>
+        <TableEditColumn
+          allowEditing
+          commandTemplate={({executeCommand, id}) => {
+          const template = commandTemplates[id];
+          if (template) {
+            
+            const onClick = (e) => {
+              executeCommand();
+              e.stopPropagation();
+            };
+            return template(onClick,);
+          }
+          return undefined;
+        }}/>
         <PagingPanel/>
       </Grid>
     );
   }
 }
 
-export default Table;
+export default withStyles(styles, {name: 'ControlledModeDemo'})(Table);
