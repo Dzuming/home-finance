3728cde807fad3eac7fb58c15477d467cb1912c7
Dawid Poliszak <dawidpoliszak@karpas.pl>
2017-10-03 19:31:15 +0200

    Add conditional rendering login  component

--- src/app.js
+++ src/app.js
@@ -7,24 +7,29 @@ import Grid from 'material-ui/Grid';
 import Nav from './containers/Navigation';
 import List from './containers/List';
 import Table from './containers/Table';
+import Login from './containers/Login';
 import './index.scss';
 import 'normalize.css';
 export default class App extends Component {
   render() {
+    const auth = false;
     return (
       <div>
         <ConnectedRouter history={createHistory()}>
-          <Grid container spacing={0}>
-            <Grid item xs={12} lg={2}>
-              <List/>
-            </Grid>
-
-            <Grid item xs={12} lg={10}>
-              <Nav/>
-              <Route exact path="/Profit" component={Table}/>
-              <Route path="/Spending" component={Table}/>
-            </Grid>
-          </Grid>
+          {!auth
+            ? (<Route exact path="/Login" component={Login}/>)
+            : (
+              <Grid container spacing={0}>
+                <Grid item xs={12} lg={2}>
+                  <List/>
+                </Grid>
+                <Grid item xs={12} lg={10}>
+                  <Nav/>
+                  <Route exact path="/Profit" component={Table}/>
+                  <Route path="/Spending" component={Table}/>
+                </Grid>
+              </Grid>
+            )}
         </ConnectedRouter>
       </div>
     );
