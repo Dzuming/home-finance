42f945b40ef9570e091c9c93339ec239b94abef8
Dawid Poliszak <dawidpoliszak@op.pl>
2017-11-21 21:07:36 +0100

    Move fetch finance flow to separate file

---
+++ __test__/fetchFinance.spec.js
@@ -0,0 +1,34 @@
+import { fetchFinanceFlow } from '../src/restApi/fetchFinance';
+import fetchMock from 'fetch-mock';
+import env from '../environments/config';
+
+describe('flow finance methods', () => {
+  afterEach(() => {
+    fetchMock.reset()
+    fetchMock.restore()
+  })
+
+  it('should get specific finance flow data', () => {
+    let body = [{
+      spending: 32,
+      category: 'jedzenie'
+    },
+    {
+      spending: 40,
+      category: 'ubrania'
+    },
+    {
+      spending: 0,
+      category: 'rachunki'
+    }]
+
+    fetchMock
+      .getOnce(`${env.api_url}/Spending/id/date`, { body, headers: { 'content-type': 'application/json' } });
+    return fetchFinanceFlow('id', 'date').then(response => {
+      expect(response[1].spending).toBe(40);
+      expect(response[2].spending).toBe(0);
+      expect(response[2].category).toBe('rachunki');
+    });
+
+  })
+})
---
+++ src/restApi/fetchFinance.js
@@ -0,0 +1,11 @@
+import env from '../../environments/config';
+
+export function fetchFinanceFlow(userId, selectedDate) {
+  const url = `${env.api_url}/Spending/${userId}/${selectedDate}`;
+  return fetch(url, { method: 'GET' }).then(response => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  });
+}
