1534f1ab363415abcf85380a5df0c91e2f52112d
Dzuming <dawidpoliszak@op.pl>
2018-01-22 21:47:25 +0100

    Add tests for Login.spec.js

--- src/containers/Login.spec.js
+++ src/containers/Login.spec.js
@@ -1,3 +1,22 @@
-test('adds 1 + 2 to equal 3', () => {
-  expect(3).toBe(3);
+import React from 'react';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import Login from './Login';
+import { Provider } from 'react-redux';
+import { create } from "react-test-renderer";
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+const store = mockStore({token: '', user: [], auth: {isAuthenticated: true}});
+const component = create(
+  <Provider store={store}>
+    <Login/>
+  </Provider>
+);
+
+describe('<Login />', () => {
+  it('should match snapshot', () => {
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
