d383f2933ca261b0641dab6dfd9acf78334375c6
Dzuming <dawidpoliszak@op.pl>
2018-02-02 18:42:35 +0100

    Add getUserId to mapper

--- src/helpers/Mappers.js
+++ src/helpers/Mappers.js
@@ -1,8 +1,9 @@
+import { getUser} from './LocalStorage';
+
 export const getFinanceFlowToSend = (data) => {
-  const currentUser = JSON.parse(localStorage.getItem('user'));
   return {
     categoryId: '59f0efc7407b78332878b47a',
-    userId: currentUser.id,
+    userId: getUser().id,
     spending: data.spending,
     description: data.description,
     dateCreated: data.dateCreated
