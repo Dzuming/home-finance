ad7f9824a08bb04f5a3a5e857ac8ce6b6abd3927
Dzuming <dawidpoliszak@op.pl>
2018-03-04 19:52:18 +0100

    Add budget actions

---
+++ src/actions/budgetActions.js
@@ -0,0 +1,17 @@
+import * as types from './actionTypes';
+import { createAction } from 'redux-actions';
+import env from '../../environments/config';
+
+export const fetchBudget = () => (dispatch, getState) => {
+  const {id} = getState().user;
+  return dispatch({
+    type: types.API_REQUEST_GET,
+    payload:
+      {
+        url: `${env.api_url}/api/budget/${id}`,
+        success: setBudget
+      }
+  });
+};
+
+export const setBudget = createAction(types.BUDGET_CRUD.READ);
---
+++ src/actions/budgetActions.spec.js
@@ -0,0 +1,12 @@
+import * as actions from './budgetActions';
+import expect from 'expect';
+
+const budget = {
+  budget: 50000
+};
+
+describe('budget actions', () => {
+  it('should create API_REQUEST_PENDING action with snapshot', () => {
+    expect(actions.setBudget(budget)).toMatchSnapshot();
+  });
+});
