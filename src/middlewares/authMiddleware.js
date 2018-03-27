7e353daac8f5472747484cf78fee33229ca32710
Dzuming <dawidpoliszak@op.pl>
2018-03-27 08:17:28 +0200

    Get user if authenticate

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,5 +1,6 @@
 import * as types from '../actions/actionTypes';
 import { setAuthToken } from '../helpers/LocalStorage';
+import { getUser } from '../actions/userActions';
 
 export const authMiddleware = ({dispatch}) => (next) => (action) => {
 
@@ -17,6 +18,7 @@ export const authMiddleware = ({dispatch}) => (next) => (action) => {
       .then(response => {
         const accessToken = response.token;
         setAuthToken(accessToken);
+        dispatch(getUser(data.email));
         dispatch(success());
       }).then(() => {
         dispatch({type: types.API_REQUEST.SUCCESS});
