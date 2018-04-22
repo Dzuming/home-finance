aa10c58f46f6dd5bb7a5aac1fd7fabb6975f1c0e
Dzuming <dawidpoliszak@op.pl>
2018-04-22 09:02:00 +0200

    Add assumption view

--- src/containers/Assumption.js
+++ src/containers/Assumption.js
@@ -1,9 +1,71 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Card, CardContent, Grid, Typography } from 'material-ui';
 
 class Assumption extends Component {
   render() {
-    return <div>test</div>;
+    return (
+      <React.Fragment>
+        <Grid container spacing={0}>
+          <Grid item xs={4}>
+            <Card>
+              <CardContent>
+                <Typography type="display3" gutterBottom align="center">
+                  Assumption
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            <Card>
+              <CardContent>
+                <Typography type="display3" gutterBottom align="center">
+                  percentage
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            <Card>
+              <CardContent>
+                <Typography type="display3" gutterBottom align="center">
+                  Value
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+        <Grid container spacing={0}>
+          <Grid item xs={4}>
+            <Card>
+              <CardContent>
+                <Typography color="inherit" type="display2" align="center">
+                  334
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            <Card>
+              <CardContent>
+                <Typography color="inherit" type="display2" align="center">
+                  445
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            <Card>
+              <CardContent>
+                <Typography color="inherit" type="display2" align="center">
+                  445
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </React.Fragment>
+    );
   }
 }
 
