27a98ed9b0133f2bdae6816daffb6a51cf1f7fe0
dpoliszak <dawidpoliszak@op.pl>
2018-04-30 05:43:35 +0200

    Prettier code

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -12,22 +12,22 @@ import path from 'path';
 
 const GLOBALS = {
   'process.env.NODE_ENV': JSON.stringify('production'),
-  __DEV__: false
+  __DEV__: false,
 };
 
 export default {
-  resolve : {
-    extensions: ['*', '.js', '.jsx', '.json']
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.json'],
   },
-  devtool : 'source-map', // more info:https://webpack.js.org/guides/production/#source-mapping and https://webpack.js.org/configuration/devtool/
-  entry : path.resolve(__dirname, 'src/index'),
-  target : 'web',
-  output : {
+  devtool: 'source-map', // more info:https://webpack.js.org/guides/production/#source-mapping and https://webpack.js.org/configuration/devtool/
+  entry: path.resolve(__dirname, 'src/index'),
+  target: 'web',
+  output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: '[name].[chunkhash].js'
+    filename: '[name].[chunkhash].js',
   },
-  plugins : [
+  plugins: [
     // Hash the files using MD5 so that their names change when the content changes.
     new WebpackMd5Hash(),
 
@@ -54,59 +54,71 @@ export default {
         keepClosingSlash: true,
         minifyJS: true,
         minifyCSS: true,
-        minifyURLs: true
+        minifyURLs: true,
       },
       inject: true,
       // Note that you can add custom options here if you need to handle other custom
       // logic in index.html To track JavaScript errors via TrackJS, sign up for a
       // free trial at TrackJS.com and enter your token below.
-      trackJSToken: ''
+      trackJSToken: '',
     }),
 
     // Minify JS
-    new webpack
-      .optimize
-      .UglifyJsPlugin({sourceMap: true}),
+    new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
 
     new webpack.LoaderOptionsPlugin({
-      minimize: true, debug: false, noInfo: true, // set to false to see a list of every file being bundled.
+      minimize: true,
+      debug: false,
+      noInfo: true, // set to false to see a list of every file being bundled.
       options: {
         sassLoader: {
-          includePaths: [path.resolve(__dirname, 'src', 'scss')]
+          includePaths: [path.resolve(__dirname, 'src', 'scss')],
         },
         context: '/',
-        postcss: () => [autoprefixer]
-      }
-    })
+        postcss: () => [autoprefixer],
+      },
+    }),
   ],
-  module : {
+  module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
-      }, {
+        loader: 'babel-loader',
+      },
+      {
         test: /\.eot(\?v=\d+.\d+.\d+)?$/,
-        loader: 'url-loader?name=[name].[ext]'
-      }, {
+        loader: 'url-loader?name=[name].[ext]',
+      },
+      {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff&name=[name].[ext]'
-      }, {
+        loader:
+          'url-loader?limit=10000&mimetype=application/font-woff&name=[name].[ext]',
+      },
+      {
         test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/octet-stream&name=[name].[ext]'
-      }, {
+        loader:
+          'url-loader?limit=10000&mimetype=application/octet-stream&name=[name].[ext]',
+      },
+      {
         test: /\.svg(\?v=\d+.\d+.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=image/svg+xml&name=[name].[ext]'
-      }, {
+        loader:
+          'url-loader?limit=10000&mimetype=image/svg+xml&name=[name].[ext]',
+      },
+      {
         test: /\.(jpe?g|png|gif)$/i,
-        loader: 'file-loader?name=[name].[ext]'
-      }, {
+        loader: 'file-loader?name=[name].[ext]',
+      },
+      {
         test: /\.ico$/,
-        loader: 'file-loader?name=[name].[ext]'
-      }, {
+        loader: 'file-loader?name=[name].[ext]',
+      },
+      {
         test: /(\.css|\.scss|\.sass)$/,
-        loader: ExtractTextPlugin.extract('css-loader?sourceMap!postcss-loader!sass-loader?sourceMap')
-      }
-    ]
-  }
+        loader: ExtractTextPlugin.extract(
+          'css-loader?sourceMap!postcss-loader!sass-loader?sourceMap',
+        ),
+      },
+    ],
+  },
 };
