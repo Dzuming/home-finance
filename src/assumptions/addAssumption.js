1952fc77068c5eba13f96b3c3c9e8272c2b11a8e
dpoliszak <dawidpoliszak@op.pl>
2018-05-11 05:39:19 +0200

    Add assumption on button click

--- src/assumptions/addAssumption.js
+++ src/assumptions/addAssumption.js
@@ -3,21 +3,29 @@ import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import * as assumptionActions from '../actions/assumptionActions';
 import { connect } from 'react-redux';
+import { Button } from 'material-ui';
 
 class AddAssumption extends Component {
-  componentWillMount() {
-    const assumption = {
+  state = {
+    assumption: {
       userId: 1,
       assumptionTypeId: 1,
       percentage: 20,
       isInitialValue: 0,
       period: '2018-04',
-    };
+    },
+  };
+  createAssumption(assumption) {
     this.props.actions.createAssumption(assumption);
   }
 
   render() {
-    return <div>test</div>;
+    const { assumption } = this.state;
+    return (
+      <Button onClick={this.createAssumption(assumption)}>
+        Add assumption
+      </Button>
+    );
   }
 }
 
